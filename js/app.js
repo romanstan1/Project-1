e18b1524203d01567176d099ce0d92458f518089
Roman Stankiewicz <roman.stan3@gmail.com>
2017-02-12 11:42:36 +0000

    walls array, responsive board and faster food generation

--- js/app.js
+++ js/app.js
@@ -1,30 +1,41 @@
 $(() => {
-  const $board = $('main.board');
+  const $board = $('section.board');
   const snake = [
-    ['col15','row12'],
-    ['col14','row12'],
-    ['col13','row12'],
-    ['col12','row12']
+    ['col19','row32'],
+    ['col18','row32'],
+    ['col17','row32'],
+    ['col16','row32'],
+    ['col15','row32'],
+    ['col14','row32'],
+    ['col13','row32'],
+    ['col12','row32']
   ];
 
   const direction = [
   ['-1', '0'], // north
   ['0', '-1'], // west
   ['1', '0'], // south
-  ['0', '1'] // east
+  ['0', '1'], // east
+  ['1', '-1'], // diagnol right up
+  ['1', '1'], // diagnol right down
+  ['-1', '1'], // diagnol left down
+  ['-1', '-1'] // diagnol left up
   ];
 
+  const winH = $(window).height();
+  const winW = $(window).width();
+  let walls = [];
   let directionKey = 3; // east
   let stopGame = null;
   let $ul = null;
   let $li = null;
   let $frontCell = null;
-  const boardWidth = 64;
-  const boardHeight = 51;
+  const boardWidth = Math.round(winW/15)+10;
+  const boardHeight = Math.round(winH/15)+6;
   let foodY = null;
   let foodX = null;
-  let food = [];
-  let time = 100;
+  const food = [];
+  const time = 100;
 
   function createBoard() {
     for (let x = 11; x < boardHeight; x++) {
@@ -41,8 +52,6 @@ $(() => {
       $li = $(`li.cell.${snake[i][0]}.${snake[i][1]}`);
       $li.addClass('snake');
     }
-    // $tailCell = $(`li.cell.${snake[snake.length-1][0]}.${snake[snake.length-1][1]}`);
-    // console.log($tailCell);
   }
 
   function snakePosition() {
@@ -51,9 +60,19 @@ $(() => {
     snake.pop();
     let col = parseInt(snake[0][0].slice(-2));
     col = col + parseInt(direction[directionKey][1]);
+    if(col>=boardWidth) {
+      col = (col - boardWidth)+11;
+    } else if (col<=10) {
+      col = (boardWidth -1);
+    }
     const colString = 'col'+col;
     let row = parseInt(snake[0][1].slice(-2));
     row = row + parseInt(direction[directionKey][0]);
+    if(row >= boardHeight) {
+      row = (row - boardHeight)+11;
+    } else if (row <= 10) {
+      row = (boardHeight - 1);
+    }
     const rowString = 'row'+row;
     snake.unshift([colString, rowString]);
     createSnake();
@@ -83,34 +102,63 @@ $(() => {
         $frontCell.addClass('snakeFront');
       }
     }
+    for (let i = 0; i<walls.length; i++) {
+      if(walls[i].includes(front[0]) && walls[i].includes(front[1])) {
+        clearInterval(stopGame);
+        $frontCell = $(`li.cell.${snake[0][0]}.${snake[0][1]}`);
+        $frontCell.addClass('snakeFront');
+      }
+    }
     snake.unshift(front);
   }
 
+  function createWalls() {
+    walls = [
+    ['col17','row11'],
+    ['col16','row11'],
+    ['col15','row11'],
+    ['col14','row11'],
+    ['col13','row11'],
+    ['col12','row11'],
+    ['col11','row11']
+    ];
+
+    for (let i = 0; i < walls.length; i++) {
+      $li = $(`li.cell.${walls[i][0]}.${walls[i][1]}`);
+      $li.addClass('walls');
+    }
+  }
+
+///// ----------------------------------------------------- FOOD FUNCTIONS
   function makeFood() {
-    foodX = (Math.ceil(Math.random()*(boardHeight-13)))+11; // 51 = boardHeight 1---38  cells: 11---49
-    foodY = (Math.ceil(Math.random()*(boardWidth-13)))+11; // 64 = boardWidth 1---51   cells:11---62
-    food.push('col'+foodY);
-    food.push('row'+foodX);
-    $li = $(`li.cell.${food[0]}.${food[1]}`);
+    foodX = (Math.ceil(Math.random()*(boardHeight-13)))+11;
+    foodY = (Math.ceil(Math.random()*(boardWidth-13)))+11;
+    //let fl = food.length;
+    food.unshift([]);
+    food[0].unshift('col'+foodY);
+    food[0].unshift('row'+foodX);
+    $li = $(`li.cell.${food[0][0]}.${food[0][1]}`);
     $li.addClass('food');
-  } // Gameboard 53 wide x 40 high
+  }
 
   function eatFood() {
     const front = snake[0];
-    if(food.includes(front[0]) && food.includes(front[1])){
-      $li = $(`li.cell.${food[0]}.${food[1]}`);
+    if(food[0].includes(front[0]) && food[0].includes(front[1])){
+      $li = $(`li.cell.${food[0][0]}.${food[0][1]}`);
       $li.removeClass('food');
       $li.addClass('foodSwallowed');
+      //food.pop();
+      makeFood();
+      console.log(food);
     }
   }
   function foodHitsSnakesTail() {
     const tail = snake[snake.length-1];
-    if(food.includes(tail[0]) && food.includes(tail[1])){
-      snake.push(food);
-      $li.removeClass('foodSwallowed');
-      food = [];
-      makeFood();
-      const $tailCell = $(`li.cell.${tail[0]}.${tail[1]}`);
+    const $tailCell = $(`li.cell.${tail[0]}.${tail[1]}`);
+    if(food[food.length-1].includes(tail[0]) && food[food.length-1].includes(tail[1])){
+      snake.push(food[food.length-1]);
+      $tailCell.removeClass('foodSwallowed');
+      food.pop();
       $tailCell.addClass('foodEaten');
       setTimeout(() => {
         $tailCell.removeClass('foodEaten');
@@ -118,9 +166,11 @@ $(() => {
     }
   }
 
+////------------------------------------------------------- INITIALIZING
   createBoard();
   createSnake();
   makeFood();
+  //createWalls();
 
   stopGame = setInterval(() => {
     snakePosition();
@@ -128,6 +178,34 @@ $(() => {
     foodHitsSnakesTail();
     isGameOver();
   }, time);
+
+/////-------------------------------- Spell out the word SNAKE with openingSequence
+
+////// DIRECTION MAP
+/////   7    0    4
+/////   1         3
+/////   6    2    5
+
+  function openingSequence() {
+    const letterDirections = [0, 1, 0, 3, 0, 0, 0, 0];
+    const letterTimes     = [10, 20, 3, 2, 18, 12, 9, 10];
+    let i = 0;
+
+    const writeSnake = function() {
+      directionKey = letterDirections[i];
+      snakeTimer = setInterval(writeSnake, time*letterTimes[i]);
+      i++;
+      if ( i-1 > letterDirections[i]) {
+        clearInterval(snakeTimer);
+        console.log('Interval CLEARED?');
+      }
+    };
+
+    let snakeTimer = setInterval(writeSnake, time*letterTimes[i]);
+
+  }
+
+  //openingSequence();
 
   $(document).keydown(arrowKeys);
 });
